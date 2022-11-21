2e98bc29f92d05e7c1657b1f777cf8df9e3b0855
Yevhenii Khovaiev <ekhovaev@gmail.com>
2022-11-21 02:01:32 +0200

    css libs optimization

--- gulpfile.js
+++ gulpfile.js
@@ -4,6 +4,8 @@ const sass = require('gulp-sass')(require('sass'));
 const rename = require("gulp-rename");
 const autoprefixer = require('gulp-autoprefixer');
 const cleanCSS = require('gulp-clean-css');
+const imagemin = require('gulp-imagemin');
+const htmlmin = require('gulp-htmlmin');
 
 // Static server
 gulp.task('server', function() {
